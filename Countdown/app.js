a1df7ed8478f359d593b196f270a6cce747a64fa
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-09-09 20:53:27 +0300

    .

--- Countdown/app.js
+++ Countdown/app.js
@@ -22,3 +22,24 @@ const weekdays = [
   'Saturday',
 ];
 
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2022,12,15,11,30,0);
+
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+
+giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year} ${hour}:${minutes}AM`;
+
+
